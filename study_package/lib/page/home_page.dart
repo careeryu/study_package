d25a6348e93f65f6c949f8c1e06a24932ab0b7b3
sangsoo <careeryu@naver.com>
2022-09-27 17:44:37 +0900

    custom toast case

--- study_package/lib/page/home_page.dart
+++ study_package/lib/page/home_page.dart
@@ -9,6 +9,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late FToast fToast;
+
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,15 +28,8 @@ class _HomePageState extends State<HomePage> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           ElevatedButton(
-            onPressed: () {Fluttertoast.showToast(
-        msg: "안녕",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.CENTER,
-        timeInSecForIosWeb: 1,
-        backgroundColor: Colors.red,
-        textColor: Colors.white,
-        fontSize: 16.0,
-    );
+            onPressed: () {
+              _showToast();
     },
             child: const Text('btn'),
           ),
@@ -35,4 +38,42 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+  void _showToast() {
+    Widget toast = Container(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+        decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Colors.greenAccent,
+        ),
+        child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: const [
+            Icon(Icons.check),
+            SizedBox(
+            width: 12.0,
+            ),
+            Text("This is a Custom Toast"),
+        ],
+        ),
+    );
+
+
+    fToast.showToast(
+        child: toast,
+        gravity: ToastGravity.BOTTOM,
+        toastDuration: const Duration(seconds: 2),
+    );
+    
+    // Custom Toast Position
+    fToast.showToast(
+        child: toast,
+        toastDuration: const Duration(seconds: 2),
+        positionedToastBuilder: (context, child) {
+          return Positioned(
+            child: child,
+            top: 16.0,
+            left: 16.0,
+          );
+        });
+}
 }
